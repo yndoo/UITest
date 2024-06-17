bf236f6c64052e4ca72f7e9e96999dae9e868ee5
yndoo 배연두 <yndoo2k@gmail.com>
2024-06-17 17:09:46 +0900

    초기 커밋.

---
+++ Source/UITest/TestHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "TestHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UITEST_API ATestHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
